d5dfc0e679fd5bb6e0853256cfc99af041d96645
Xavier Clerc <xclerc@janestreet.com>
2020-04-21 09:37:28 +0100

    v0.14-preview.122.18+07

--- test/slow/list.ml
+++ test/slow/list.ml
@@ -6,7 +6,7 @@ let%expect_test "no stack overflow on List.each and List.eachi" =
   let xs =
     List.init large_enough_to_trigger_the_stack_overflow_bug_that_used_to_exist ~f:Fn.id
   in
-  Expect_test_helpers_kernel.require_does_not_raise ~cr:CR_soon [%here] (fun () ->
+  Expect_test_helpers_core.require_does_not_raise ~cr:CR_soon [%here] (fun () ->
     Accessor.iter Accessor.List.each xs ~f:(fun (_ : int) -> ());
     Accessor.iteri Accessor.List.eachi xs ~f:(fun [ index ] value ->
       [%test_eq: int] index value));
