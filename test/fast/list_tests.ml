c70d151969c9a1870ad5617ed8ce35387eea7b46
public-release <as-public-release@janestreet.com>
2024-11-08 22:02:20 +0000

    v0.18~preview.130.04+450

--- test/fast/list_tests.ml
+++ test/fast/list_tests.ml
@@ -57,3 +57,42 @@ let%test_unit "each" =
     (module Unit)
     (fun () -> Accessor.List.each)
 ;;
+
+let%expect_test "monadic fold" =
+  let module M = struct
+    module T0 = struct
+      type 'a t =
+        | Return of 'a
+        | Effect of bool * 'a t
+      [@@deriving compare, sexp_of, quickcheck]
+
+      let return a = Return a
+      let map = `Define_using_bind
+
+      let rec bind t ~f =
+        match t with
+        | Return a -> f a
+        | Effect (n, t) -> Effect (n, bind t ~f)
+      ;;
+    end
+
+    module T1 = struct
+      include T0
+      include Monad.Make (T0)
+
+      let apply = `Define_using_bind
+    end
+
+    include T1
+    include Accessor.Of_monad (T1)
+  end
+  in
+  Quickcheck.test
+    [%quickcheck.generator: bool list * bool * (bool -> bool -> bool M.t)]
+    ~f:(fun (xs, init, f) ->
+      [%test_result: bool M.t]
+        (M.fold Accessor.List.each xs ~init ~f)
+        ~expect:
+          (List.fold xs ~init:(M.return init) ~f:(fun acc a ->
+             M.bind acc ~f:(fun acc -> f acc a))))
+;;
